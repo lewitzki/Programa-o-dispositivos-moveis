1738ddc9500f919dc889ffcfc2692802e61a5d52
Pedro Matozo <matozopedro8@gmail.com>
2025-09-13 10:26:42 -0300

    atividade2

---
+++ atividade2/lib/main.dart
@@ -0,0 +1,29 @@
+void main() {
+  
+  String nome = "Pedro";
+  int idade = 20;
+  double altura = 1.75;
+  bool estudaFlutter = true;
+
+  if (idade >= 18) {
+    print("Você é maior de idade.");
+  } else {
+    print("Você é menor de idade.");
+  }
+
+  String apresentarAluno(String nome, int idade, bool estudaFlutter) {
+    return "Olá, meu nome é $nome, tenho $idade anos e estou aprendendo Flutter: $estudaFlutter";
+  }
+
+  print(apresentarAluno(nome, idade, estudaFlutter));
+
+  List<String> linguagens = ["Dart", "JavaScript", "Python"];
+  Map<String, int> materias = {
+    "Estrutura de Dados": 30,
+    "Banco de Dados": 25,
+    "Algoritmos": 40
+  };
+
+  print("Linguagens que gosto: $linguagens");
+  print("Matérias e horas estudadas: $materias");
+}
